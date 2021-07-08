005eae8d1aa292e8d189183811cedc76b527193b
Wesley Canaam <wcanaam@gmail.com>
2021-07-08 15:08:19 -0300

    Fluent API
    Configurando as entidades usando a Fluent API

---
+++ Wcanaam.CleanArchitectureBasic.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Infra.Data.EntitiesConfiguration {
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category> {
+
+        public void Configure(EntityTypeBuilder<Category> builder) {
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(50) 
+                .IsRequired();
+
+            builder.HasData(
+                new Category(1, "Electronics"),
+                new Category(1, "Accessories"),
+                new Category(1, "Candy")
+                );
+        }
+    }
+}
---
+++ Wcanaam.CleanArchitectureBasic.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Infra.Data.EntitiesConfiguration {
+    class ProductConfiguration : IEntityTypeConfiguration<Product> {
+
+        public void Configure(EntityTypeBuilder<Product> builder) {
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(150);
+
+            builder.Property(p => p.Price)
+                .HasPrecision(10,2)
+                .IsRequired();
+
+
+            builder.HasOne(r => r.Category)
+                .WithMany(e => e.Products)
+                .HasForeignKey(e => e.CategoryId); 
+             
+
+        }
+    }
+}
