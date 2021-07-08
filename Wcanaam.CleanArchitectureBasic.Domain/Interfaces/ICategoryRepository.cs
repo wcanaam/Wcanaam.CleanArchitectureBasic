f8e56829ee11bdbdd0ac59688db190d6369ceebb
Wesley Canaam <wcanaam@gmail.com>
2021-07-08 09:01:57 -0300

    Repository
    Criando as interfaces do repositório

---
+++ Wcanaam.CleanArchitectureBasic.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Domain.Interfaces {
+    public interface ICategoryRepository {
+
+        Task <IEnumerable<Category>> CategoriesAsync();
+
+        Task<Category> GetByIdAsync(int? id);
+
+        Task<Category> CreateAsync(Category category);
+
+        Task<Category> UpdateAsync(Category category);
+
+        Task<Category> DeleteAsync(Category category);
+
+
+
+
+    }
+}
---
+++ Wcanaam.CleanArchitectureBasic.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Domain.Interfaces {
+    public interface IProductRepository {
+
+        //Sufixo Async nas assinaturas dos métodos, pois é uma recomendação
+        Task<IEnumerable<Product>> ProductsAsync();
+
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+
+        Task<Product> UpdateAsync(Product product);
+
+        Task<Product> DeleteAsync(Product product);
+    }
+}
