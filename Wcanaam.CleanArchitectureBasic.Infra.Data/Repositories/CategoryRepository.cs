1c7937718fa6ce283222be4e28da6964a12560b3
Wesley Canaam <wcanaam@gmail.com>
2021-07-08 16:24:15 -0300

    Repository
    Implementando o repositório da categoria e do produto.

---
+++ Wcanaam.CleanArchitectureBasic.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+using Wcanaam.CleanArchitectureBasic.Domain.Interfaces;
+using Wcanaam.CleanArchitectureBasic.Infra.Data.Context;
+
+namespace Wcanaam.CleanArchitectureBasic.Infra.Data.Repositories {
+    public class CategoryRepository : ICategoryRepository {
+
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context) {
+            _context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category) {
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> DeleteAsync(Category category) {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category) {
+
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> CategoriesAsync() {
+
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(int? id) {
+
+            return await _context.Categories.FindAsync(id);
+        }
+
+    }
+}
---
+++ Wcanaam.CleanArchitectureBasic.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+using Wcanaam.CleanArchitectureBasic.Domain.Interfaces;
+using Wcanaam.CleanArchitectureBasic.Infra.Data.Context;
+
+namespace Wcanaam.CleanArchitectureBasic.Infra.Data.Repositories {
+    public class ProductRepository : IProductRepository {
+
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context) {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product) {
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> DeleteAsync(Product product) {
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product) {
+
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> ProductsCategoryAsync(int? id) {
+
+            return await _context.Products.Include(c => c.Category).SingleOrDefaultAsync(p => p.Id == id); 
+                
+        }
+
+        public async Task<IEnumerable<Product>> ProductsAsync() {
+
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int? id) {
+
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
