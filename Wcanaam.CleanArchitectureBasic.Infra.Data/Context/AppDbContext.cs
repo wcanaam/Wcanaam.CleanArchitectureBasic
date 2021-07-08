ed86ed07672ee38aad520ff0b6077dbccc6806d9
Wesley Canaam <wcanaam@gmail.com>
2021-07-08 14:43:36 -0300

    Inicial
    Criando classe de contexto

---
+++ Wcanaam.CleanArchitectureBasic.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Infra.Data.Context {
+    public class AppDbContext : DbContext {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
