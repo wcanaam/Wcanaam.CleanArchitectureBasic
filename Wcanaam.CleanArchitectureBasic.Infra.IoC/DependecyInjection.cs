61e8d10fafcec6347089aa06abad8f6f44175a10
Wesley Canaam <wcanaam@gmail.com>
2021-07-08 16:56:04 -0300

    Context e Repository
    Registrando os serviços

---
+++ Wcanaam.CleanArchitectureBasic.Infra.IoC/DependecyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wcanaam.CleanArchitectureBasic.Domain.Interfaces;
+using Wcanaam.CleanArchitectureBasic.Infra.Data.Context;
+using Wcanaam.CleanArchitectureBasic.Infra.Data.Repositories;
+
+namespace Wcanaam.CleanArchitectureBasic.Infra.IoC {
+    public  static class DependecyInjection {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SqlServer"),
+                b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
