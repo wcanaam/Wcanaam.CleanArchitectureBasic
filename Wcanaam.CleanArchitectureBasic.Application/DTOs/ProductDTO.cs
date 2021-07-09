5a07b8ba187992df91b128b9fa24e32177ceca85
Wesley Canaam <wcanaam@gmail.com>
2021-07-09 05:05:07 -0300

    DTO e AutoMapper
    Criando os DTOs e os mapeamentos com o AutoMapper

---
+++ Wcanaam.CleanArchitectureBasic.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Application.DTOs {
+    public class ProductDTO {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string Name { get; private set; }
+
+        [MaxLength(150)]
+        public string Description { get; private set; }
+
+        [Required(ErrorMessage = "The price is required")]
+        [Column(TypeName = "decimal(10, 2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; private set; }
+
+        [Required(ErrorMessage = "The price is required")]
+        [Range(1, 999)]
+        public int Stock { get; private set; }
+
+        [Display(Name = "Categories")]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
---
+++ Wcanaam.CleanArchitectureBasic.Application/Mappings/DomainToDTIOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Wcanaam.CleanArchitectureBasic.Application.DTOs;
+using Wcanaam.CleanArchitectureBasic.Domain.Entities;
+
+namespace Wcanaam.CleanArchitectureBasic.Application.Mappings {
+    public class DomainToDTIOMappingProfile : Profile {
+
+        public DomainToDTIOMappingProfile() {
+
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
